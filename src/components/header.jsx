0c34a3cc01a5d79298e529c5697c5aad6542a833
somesh <someshsrichandan@gmail.com>
2024-08-21 10:02:15 +0530

    user icon and drop down in header added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Link2, Link2Icon, LogOutIcon } from 'lucide-react'
 
 const Header = () => {
   const navigate = useNavigate()
+  const user = true;
   return (
     <nav className='flex items-center justify-between py-4'>
       <Link to='/' >
@@ -11,7 +22,27 @@ const Header = () => {
       </Link>
 
       <div>
-        <Button onClick={()=>navigate("/auth")}>Login</Button>
+        {!user ? <Button onClick={() => navigate("/auth")}>Login</Button>
+          : <DropdownMenu>
+            <DropdownMenuTrigger className='w-10 overflow-hidden rounded-full'><Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Somesh Srichandan</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link2Icon className='w-4 h-4 mr-2' />
+                links</DropdownMenuItem>
+              <DropdownMenuItem className='text-red-400'>
+                <LogOutIcon className='w-4 h-4 mr-2' />
+                <span>Logout</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        }
+
       </div>
 
     </nav>
