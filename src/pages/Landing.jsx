a31690877a5b3ab5e964f9e4146f75b2a8c45d7c
somesh <someshsrichandan@gmail.com>
2024-08-22 23:34:22 +0530

    lading page changes and heading added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div className=''>Landing</div>
+    <div className='flex flex-col items-center'>
+      <h2 className='my-10 text-3xl font-extrabold text-center text-white sm:my-16 sm:text-6xl lg:text-7xl'>
+        The only URL Shortener <br/>you 🔗ll ever need! 👇
+      </h2>
+
+    </div>
   )
 }
 
