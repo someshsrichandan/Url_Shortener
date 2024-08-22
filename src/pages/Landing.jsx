9426cf7e2f56bb2d836c29f2f0fb73507fe7d764
somesh <someshsrichandan@gmail.com>
2024-08-22 23:47:17 +0530

    landing page image added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,18 +1,27 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import React from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 
 const Landing = () => {
   return (
     <div className='flex flex-col items-center'>
       <h2 className='my-10 text-3xl font-extrabold text-center text-white sm:my-16 sm:text-6xl lg:text-7xl'>
-        The only URL Shortener <br/>you 🔗ll ever need! 👇
+        The only URL Shortener <br />you 🔗ll ever need! 👇
       </h2>
       <form className='flex flex-col w-full gap-2 sm:h-14 sm:flex-row md:w-2/4'>
-          <Input placeholder='Enter your Loooooooooong URL here' type='url' value="" className='flex-1 h-full p-4' />
-          <Button className='h-full' type='submit' variant="destructive">Shorten</Button>
+        <Input placeholder='Enter your Loooooooooong URL here' type='url' value="" className='flex-1 h-full p-4' />
+        <Button className='h-full' type='submit' variant="destructive">Shorten</Button>
       </form>
+      <img src='/Short-URl.svg' className='w-full my-11 md:px-11' alt='illustration' />
       
+
     </div>
   )
 }
