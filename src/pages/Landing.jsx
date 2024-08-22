776aae51ba1cc60f44713cb59de63a876cd84a87
somesh <someshsrichandan@gmail.com>
2024-08-22 23:35:39 +0530

    landing page form added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,6 +6,9 @@ const Landing = () => {
       <h2 className='my-10 text-3xl font-extrabold text-center text-white sm:my-16 sm:text-6xl lg:text-7xl'>
         The only URL Shortener <br/>you 🔗ll ever need! 👇
       </h2>
+      <form className='flex flex-col w-full gap-2 sm:h-14 sm:flex-row md:w-2/4'>
+
+      </form>
 
     </div>
   )
