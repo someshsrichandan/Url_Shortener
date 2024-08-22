70692be0cc3cb087c7be7dd5e7feacefd7f9a294
somesh <someshsrichandan@gmail.com>
2024-08-22 23:51:22 +0530

    input field fixed

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -16,8 +16,8 @@ const Landing = () => {
       <h2 className='my-10 text-3xl font-extrabold text-center text-white sm:my-16 sm:text-6xl lg:text-7xl'>
         The only URL Shortener <br />you 🔗ll ever need! 👇
       </h2>
-      <form className='flex flex-col w-full gap-2 sm:h-14 sm:flex-row md:w-2/4'>
-        <Input placeholder='Enter your Loooooooooong URL here' type='url' value="" className='flex-1 h-full p-4' />
+      <form onSubmit={handelShorner()} className='flex flex-col w-full gap-2 sm:h-14 sm:flex-row md:w-2/4'>
+        <Input placeholder='Enter your Loooooooooong URL here' type='url' value={longUrl} className='flex-1 h-full p-4' />
         <Button onChange={(e)=>{setLongUrl(e.target.value)}} className='h-full' type='submit' variant="destructive">Shorten</Button>
       </form>
       <img src='/Short-URl.svg' className='w-full my-11 md:px-11' alt='illustration' />
