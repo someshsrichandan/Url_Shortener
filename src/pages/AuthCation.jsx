5b9c0c8aa6826995adc367237f8d43c8eebb6099
somesh <someshsrichandan@gmail.com>
2024-08-23 11:28:25 +0530

    login signup section added

--- src/pages/AuthCation.jsx
+++ src/pages/AuthCation.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Login from '@/components/login'
+import SignUp from '@/components/signup'
 
 const AuthCation = () => {
+  const [searchParams] = useSearchParams()
   return (
-    <div>AuthCation</div>
+    <div className='flex flex-col items-center gap-10 mt-36'>
+      <h1 className='text-5xl font-extrabold'>
+        {searchParams.get('createNew') ? "Hold up! let's Login First" : 'Login / Signup'}
+      </h1>
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">SignUp</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login"><Login/></TabsContent>
+        <TabsContent value="Signup"><SignUp/></TabsContent>
+      </Tabs>
+
+    </div>
   )
 }
 
